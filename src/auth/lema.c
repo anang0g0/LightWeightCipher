4bc2e2cfa9dfc15e5204850059243f6a581860a2
Osamu Sakai <fumumue@protonmail.com>
2021-05-04 23:14:48 +0900

    begin

--- src/auth/lema.c
+++ src/auth/lema.c
@@ -13,8 +13,6 @@
 
  void rp(unsigned char* a) {
         int i, j, x;
-        time_t t;
-
 
 
         for (i = 0; i < N; i++) {
@@ -34,7 +32,6 @@
             a[N - 2] = N - 1;
         }
 
-
  }
 
 
@@ -42,11 +39,11 @@
 int main(){
   
     int i,j;
-    int t[N]={0},t2[N]={0},t3[N]={0}; //public
+    unsigned char t[N]={0},t2[N]={0}; //public
     unsigned char u[N]; //public
-    int ss[N]={0},r[N]={0},z[N]={0},e[N],e2[N],xx[N]={0};
-    int v[N]={0};
-    unsigned int l[N]={0},m[N]={0},n[N]={0},ll=0,nn=0;
+    unsigned char ss[N]={0},r[N]={0},z[N]={0},e[N],e2[N],xx[N]={0};
+    unsigned char v[N]={0};
+    unsigned int l[N]={0},n[N]={0};
     time_t tt;
 
     srand(clock()+time(&tt));
@@ -66,35 +63,31 @@ v[i]=e[i]^e2[i];
 
 
   for(j=0;j<N;j++){
-    for(i=0;i<N;i++){
-      t[i]=(ss[i]^ss[(i+1)%N]);
-    }
 
     for(i=0;i<N;i++)
+      t[i]=(ss[i]^ss[(i+1)%N]);
+    for(i=0;i<N;i++)
       r[i]=ss[u[i]];
-
     memcpy(ss,r,sizeof(ss));
 printf("\n");
 
 
-    for(i=0;i<N;i++){
+    for(i=0;i<N;i++)
       t2[i]=(xx[i]^xx[(i+1)%N]);
-    }
-
-    for(i=0;i<N;i++) 
+    for(i=0;i<N;i++){ 
+      r[i]=0;
       r[i]=xx[u[i]];
-
+    }
       memcpy(xx,r,sizeof(xx));
 printf("\n");
 
 
     for(i=0;i<N;i++)
-      z[i]=(v[i]^v[(i+1)%N]);      
-    
-
-    for(i=0;i<N;i++)
+      z[i]=(v[i]^v[(i+1)%N]);
+    for(i=0;i<N;i++){
+      r[i]=0;
       r[i]=v[u[i]];
-
+    }
       memcpy(v,r,sizeof(v));
 printf("\n\n");
 
@@ -113,11 +106,12 @@ printf("\n\n");
 */
 
 for(i=0;i<N;i++){
-  printf("%d,%d\n",t[i]^t2[i],z[i]);
+  //printf("%d,%d\n",t[i]^t2[i],z[i]);
   l[j]+=t[i]^t2[i];
   n[j]+=z[i];
   l[j]=(l[j]<<1);
   n[j]=(n[j]<<1);
+    printf("%d,%d\n",l[i],n[i]);
   }
   
 l[j]=(l[j]>>1);
