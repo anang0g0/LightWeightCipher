dfc6259c6cea8ca72af4f797e746563199844ad4
Osamu Sakai <fumumue@protonmail.com>
2021-05-06 23:00:19 +0900

    bugfix

--- src/global.h
+++ src/global.h
@@ -3,39 +3,33 @@
 #include <string.h>
 //#include "8192.h"
 
-
-
 //符号のパラーメータの指定。通常[N,K,T]として、
 //Nは符号の長さ、Kが符号の次元、Tは訂正エラー数
 //を表す。ここではDは符号長にしている。
-#define N 16 // (^^;)?
-#define M 16 //有限体の元の数
-#define K (6) //符号の次元
-#define DEG (K*2)
-#define T (K/2) //エラーの数
-#define E (4) //拡大体のビット数
-#define D (16) //符号長（短縮符号）
-#define F  E*K //2040
+#define N 256  // (^^;)?
+#define M 256 //有限体の元の数
+#define K (16) //符号の次元
+#define DEG (K * E)
+#define T (K / 2) //エラーの数
+#define E (8)    //拡大体のビット数
+#define D (256)  //符号長（短縮符号）
+//#define F E *K    //2040
+#define NN 16
 
 
+unsigned char tmp[N][E * K] = {0};
+//unsigned char pub[E * K][N] = {0};
+//unsigned char BH[E * K][N] = {0};
+static unsigned short c[E * K + 1] = {0};
+unsigned short mat[N][K*E] = {0};
+unsigned short ma[N][K*E] = {0};
+unsigned short bm[N][K*E]={0};
+unsigned short bm2[N][K*E]={0};
 
-unsigned char tmp[E * K][N]={0};
-unsigned char pub[E * K][N]={0};
-unsigned char BH[E * K][N]={0};
-static unsigned short c[2 * K + 1]={0};
-static unsigned short mat[N][K]={0};
-
-
-/*
-unsigned char a[F][F]={0};
-unsigned char cc[F][F]={0};
-unsigned char bb[F][F]={0};
-unsigned char cl[F][F]={0};
-*/
+unsigned char x0[NN]={0};
+unsigned char x1[NN]={0};
 
 //unsigned short syn[K]={0};
-unsigned char A[N][N]={0};
-unsigned short P[N]={0};
-unsigned short inv_P[N]={0};
-unsigned short uu=0;
-
+unsigned short P[N] = {0};
+unsigned short inv_P[N] = {0};
+unsigned short uu = 0;
