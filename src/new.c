fb2c8b91a93ee669c3798b5fcfa452d3a0e84418
Osamu Sakai <fumumue@protonmail.com>
2021-05-07 04:08:19 +0900

    modified

--- src/new.c
+++ src/new.c
@@ -3,21 +3,40 @@
 #include "chash.c"
 
 
+//324653895787272357988410799554010136716 225417171295519776360510534481775431469 
+//real	1m12.468s
+//324653895787272357988410799554010136716 225417171295519776360510534481775431469 
+//real	1m21.364s
 
 
 int main(){
-    arrayul aa={0};
+    arrayul aa={0},n={0};
     int count=0,i;
+      time_t t;
+
+
+  rp2(x0);
+  rp2(x1);
+//exit(1);
+
+n.x[0]=65535; //(unsigned int)time(&t);
+n.x[1]=65535; //(unsigned long)clock();
+
+for(i=0;i<NN;i++)
+key[i]=n.d[i];
+
+  for (i = 0; i < NN; i++)
+  inv_x[x0[i]] = i;
 
     while(1){
         //i=rand();
         aa=chash();
-        //for(i=0;i<NN/16;i++){
-        //print_uint128(aa.z[i]);
-        //printf(" ");
-        //}
-        //printf("\n");
-        //printf("%llu %llu\n",aa.u[0],aa.u[1]);
+        for(i=0;i<NN/16;i++){
+        print_uint128(aa.z[i]);
+        printf(" ");
+        }
+        printf("\n");
+        
         count++;
         if(count==10000000)
         break;
