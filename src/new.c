6b6dc069e346041d92d6a133320b2c00df6d78ab
Osamu Sakai <fumumue@protonmail.com>
2021-05-08 19:05:54 +0900

    chacha20

--- src/new.c
+++ src/new.c
@@ -2,63 +2,63 @@
 #include "struct.h"
 #include "chash.c"
 
-//324653895787272357988410799554010136716 225417171295519776360510534481775431469
-//real	1m12.468s
-//324653895787272357988410799554010136716 225417171295519776360510534481775431469
-//real	1m21.364s
+
 
 int main()
 {
-    //arrayul aa={0},
-    arrayul n = {0};
-    arrayul key = {0};
+    arrayul key={0}; 
+    unsigned char salt[NN]= {125,166,108,148,136,242,113,68,172,152,19,72,49,199,89,13,23,210,214,187,77,68,204,4,150,239,243,60,165,236,121,206,226,180,26,143,162,169,124,58,94,148,232,95,227,204,18,170,34,249,221,20,138,84,147,71,131,190,225,166,114,133,31,252};
     int count = 0, i;
-    unsigned char a = 111, b = 222, c;
-    // time_t t;
-
-    c = a * b;
-    printf("%d\n", c);
-    c = a * b % 256;
-    printf("%d\n", c);
-    //exit(1);
+    //time_t t;
 
     rp2(x0);
     rp2(x1);
     //exit(1);
+    memcpy(key.d,salt,sizeof(salt));
+    //key.u[0] = 0xdeadbeef; //4567890; //(unsigned int)time(&t);
+    //key.u[1] = 0xff11face;        //ff11face; //9876543210; //(unsigned long)clock();
+    printf("%lld, %lld\n", key.u[0], key.u[1]);
+    nonce.u[0]=3; //0x1234567890abcdef;
+    
 
-    key.x[0] = 12345; //(unsigned int)time(&t);
-    key.x[1] = 54321; //(unsigned long)clock();
+    for (i = 0; i < NN; i++)
+    {
+        printf("%d,", key.d[i]);
+    }
+    printf("\n");
 
+    for (i = 0; i < NN; i++)
+        inv_x[x0[i]] = i;
 /*
+    for (i = 0; i < 8; i++)
+    {
+        chash(&key);
+    }
     for (i = 0; i < NN; i++)
         printf("%d,", key.d[i]);
     printf("\n");
 */
-    //for(i=0;i<NN/16;i++)
-    //key.z[i]=n.z[i];
-
-    for (i = 0; i < NN; i++)
-        inv_x[x0[i]] = i;
+    //exit(1);
 
     while (1)
     {
-    for (i = 0; i < NN; i++)
-        printf("%d,", key.d[i]);
-    printf("\n");
-
         //i=rand();
+        for (i = 0; i < NN; i++)
+            z[i] = x0[x1[inv_x[i]]];
+
         chash(&key);
-        /*
-        for(i=0;i<NN/16;i++){
-        print_uint128(key.z[i]);
-        printf(" ");
-        }
-        printf("\n");
-        */
+
         count++;
         if (count == 10000000)
             break;
     }
+
+    for (i = 0; i < NN / 16; i++)
+    {
+        print_uint128(key.z[i]);
+        printf(" ");
+    }
+    printf("\n");
 
     return 0;
 }
