a4267f898f423ca0236fbf94dce9d7c80bbfa74b
Osamu Sakai <fumumue@protonmail.com>
2021-06-11 17:03:23 +0900

    cipher

--- src/sakai_permutation_rand.c
+++ src/sakai_permutation_rand.c
@@ -3,32 +3,12 @@
 #include <time.h>
 #include <string.h>
 
-#define N 8
-#define BIT_VERSION
+#define N 32
+#define BIT_VERSION 0
 
-
-	static unsigned char x[N];
-	static unsigned char y[N];
-	static unsigned char inv_x[N];
-
-unsigned int
-xor (void)
-{
-  static unsigned int y = 2463534242;
-  y^=rand();
-  y = y ^ (y << 13);
-  y = y ^ (y >> 17);
-  return y = y ^ (y << 15);
-}
-
-unsigned long long int
-xor64 (void)
-{
-  static unsigned long long int x = 88172645463325252ULL;
-  x = x ^ (x << 13);
-  x = x ^ (x >> 7);
-  return x = x ^ (x << 17);
-}
+typedef struct {
+	unsigned char x[N];
+} arr;
 
 void random_permutation(unsigned char* a){
 	int i,j,x;
@@ -50,44 +30,46 @@ void random_permutation(unsigned char* a){
 	}
 }
 
-
-unsigned long long int toInt(unsigned char * a){
-	unsigned long long int i = 0, s = 1, ret = 0;
+unsigned int toInt(unsigned char * a){
+	unsigned int i = 0, s = 1, ret = 0;
 	for(i=0; i < N; i++){
-			
 		ret += s * a[i];
 		s *= 2;
 		ret << 1;
-		}
-		//ret >>= 1;
-
+	}
 	return ret;
 }
 
-unsigned long long int p_rand(){
-#ifdef BIT_VERSION
-	static unsigned char a[N] = {
-		1,0,1,0,0,0,0,0,
-		//1,1,1,1,1,1,1,1,
-		//0,0,0,0,0,0,0,0,
-		//0,0,0,0,0,0,0,0,
-		//1,1,1,1,1,1,1,1,
-		//1,1,1,1,1,1,1,1,
-		//0,0,0,0,0,0,0,0,
-		//0,0,0,0,0,0,0,0,
-	};
-#else
-	static unsigned char a[N] = {
+void keygen(){
+	unsigned char key[N]={0};
+	int i;
+	FILE *fp;
+
+	for(i=0;i<N;i++)
+	key[i]=rand()%256;
+	fp=fopen("sec.key","wb");
+	fwrite(key,1,N,fp);
+	fclose(fp);
+}
+
+arr p_rand(){
+	static unsigned char a[N];
+	FILE *fp;
+	unsigned long long int u64=0;
+	arr xx={0};
+
+
+	/*{
 		1,2,3,5,7,11,13,17,
 		19,23,29,31,33,37,39,41,
 		143,151,157,159,161,167,171,175,
 		212,85,1,0,0,0,0,0,
 	};
-#endif
+*/
 
-	//static unsigned char x[N];
-	//static unsigned char y[N];
-	//static unsigned char inv_x[N];
+	static unsigned char x[N];
+	static unsigned char y[N];
+	static unsigned char inv_x[N];
 	static unsigned char first = 1;
 
 	static unsigned char tmp[N];
@@ -95,25 +77,26 @@ unsigned long long int p_rand(){
 
 	if(first){
 		first = 0;
+		fp=fopen("sec.key","rb");
+		fread(a,1,32,fp);
+		fclose(fp);
 
 		random_permutation(x);
 		random_permutation(y);
 
 		for(i=0;i<N;i++){
-		//	printf("%d,",x[i]);
 			inv_x[x[i]]=i; 
 		}		
-		//printf("\n");
 	}
-
-		for(i=0;i<N;i++)
-		printf("%d,",tmp[i]);
-		printf("\n");
 
 	// a ^= a * y
 	memcpy(tmp, a, sizeof(tmp)); 	// tmp = a;
 	for(i=0;i<N;i++){ 
 		a[i] ^= tmp[y[i]];
+	}
+	for(i=0;i<8;i++){
+		u64=(u64<<8);
+		xx.x[i]^=a[i];
 	}
 
 	// y = x * y * ~x
@@ -122,66 +105,50 @@ unsigned long long int p_rand(){
 	}
 	memcpy(y, tmp, sizeof(tmp));
 
-	return toInt(a);
+
+
+	return xx; //toInt(a);
 }
 
-int main(){ 
-	FILE *fp = fopen("out.txt","wb");  
-	unsigned int i,j=0,count=0,nn=0;
-	unsigned short a,b,c,d,v=0; 
-	//unsigned char a[4]={0,1,0,1};
-	//unsigned long long int 
-	a = p_rand();
-	time_t t;
+int main(int argc,char *argv[]){ 
+	//FILE *fp = fopen("README.md","rb");
+	FILE *fp = fopen(argv[1],"rb");
+	FILE *op = fopen(argv[2],"wb");
+	int i,j=0,c;
+	unsigned char pass[32]={0};
+	unsigned char buf[8]={0};
+	unsigned long long int uu=0;
 
-	//srand(time(&t)); 
-	printf("%d\n",a);
-		//random_permutation(x);
-		//random_permutation(y);
-		/*
-		for(i=0;i<N;i++){
-			inv_x[x[i]]=i; 
-		}		
-		printf("\n");
-	*/
-	b=p_rand();
-	while(j<0xffff){
-		c=p_rand();	
-		d=p_rand();	
-		printf(" c=%d d=%d %d\n",c,d,j);
-		if(a==c && b==d){
-		printf(" f1=%d %d\n",a,b);
-		nn++;
+	scanf("%u",&uu);
+//	exit(1);
+	srand(uu);
+	uu=0;
+	//keygen();
+	//exit(1);
+	while((c=fread(buf,1,8,fp))>0){
+		
+		arr a = p_rand();
+		for(i=0;i<8;i++){
+		//printf("%u",buf[i]);
+		//uu=(uu<<8);
+		//uu^=buf[i];
+		buf[i]^=a.x[i]; //rand()%256;
 		}
-		if(nn>2)
-		break;
 
-		if(a%2==0)
-		count++;
-		if(j==60000){
-			a=p_rand();
-			printf("f2=%d\n",a);
-			b=p_rand();
-			nn=0;
-			v++;
-			j=0;
-		}
-		if(v==3){
-		for(i=0;i<N;i++)
-		printf("%d,",x[i]);
-		printf("\n");
-		exit(1);
-		}
-		/*
-		if(fp){
-			fprintf(fp, "%u\n", a);
+		//printf("%llu\n",a);
+		//uu^=a;
+
+		fwrite(buf,1,c,op);
+/*
+		if(op){
+			fprintf(op, "%llu\n", uu);
 		}else{
-			printf("%u\n", a);
+			printf("%llu\n", uu);
 		}
-	*/
+*/
 		j++; 
 	} 
-	printf("count=%d %d %d\n",count,j,nn);
+
 
 	return 0;
 } 
